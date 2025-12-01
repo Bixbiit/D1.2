fcb39bb9b34fcf607cbaa3bda4f429648153d435
Bixbiit <nikfaircopy@gmail.com>
2025-12-01 17:21:30 +0300

    2

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+const humans = [{name: 'nik', age: 29,}]
+
 app.get('/', (req, res) => {
   res.send('Ку, всем нашим, остальным соболезную(');
 });
+
+app.get('/humans', (req, res) => {
+res.json(humans)
+})
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
